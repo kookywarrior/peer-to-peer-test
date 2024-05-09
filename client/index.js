52dade63349248caa2f1d89059f36684d549c400
KOOKY WARRIOR <91371138+kookywarrior@users.noreply.github.com>
2024-05-09 21:05:23 +0800

    Update index.js

--- client/index.js
+++ client/index.js
@@ -11,9 +11,11 @@
 	const output = document.getElementById("output")
 	const connect = document.getElementById("connect")
 	const send = document.getElementById("send")
+	const ping = document.getElementById("ping")
 
 	window.peer = new window.peerjs.Peer()
 	var conn = null
+	var pingTimer = null
 
 	peer.on("open", (id) => {
 		loading.hidden = true
@@ -26,20 +28,44 @@
 		conn.on("open", () => {
 			connect.hidden = true
 			send.hidden = false
+			ping.hidden = false
 			output.hidden = false
 			input.value = ""
 			input.placeholder = "Enter Message"
 			output.value = "Connected to Host\n"
 
 			conn.on("data", (data) => {
-				output.value += `Received: ${data}\n`
+				const needScroll = output.scrollHeight - output.scrollTop <= output.clientHeight + 20
+
+				if (data === "pong" && pingTimer) {
+					const time = performance.now() - pingTimer
+					output.value += `Ping: ${time}ms\n`
+					pingTimer = null
+					conn.send(`ping ${time}ms`)
+				} else {
+					output.value += `Received: ${data}\n`
+				}
+
+				if (needScroll) {
+					output.scrollTop = output.scrollHeight
+				}
 			})
 		})
 	}
 
 	send.onclick = () => {
 		conn.send(input.value)
+
+		const needScroll = output.scrollHeight - output.scrollTop <= output.clientHeight + 20
 		output.value += `Sent: ${input.value}\n`
+		if (needScroll) {
+			output.scrollTop = output.scrollHeight
+		}
 		input.value = ""
+	}
+
+	ping.onclick = () => {
+		pingTimer = performance.now()
+		conn.send("ping")
 	}
 })()
